16d599dc44d0cee438136978c9a6fa0c7e51e6e8
Lucas-Sambati <lucasleonardosambati@hotmail.com>
2024-08-22 17:02:06 -0300

    Atualizando telaInicial.html

--- telaInicial.php
+++ telaInicial.php
@@ -3,6 +3,7 @@
 
     //print_r($_SESSION);
 
+    include_once('config.php');
     if((!isset($_SESSION['email']) == true) and (!isset ($_SESSION['senha']) == true))
     {
         unset($_SESSION['email']);
@@ -10,17 +11,47 @@
         header('Location: telaLogin.php');
     }
     $logado = $_SESSION['email']; 
+
+    $sql = "SELECT nome FROM usuarios WHERE email = '$logado'";
+    $result = $conexao->query($sql);
+    $row = $result->fetch_assoc();
+    $nome = $row['nome'];
+
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FitNation</title>
+
+    <link rel="shortcut icon" type="imagex/png" href="assets/img/icone.ico">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cambo&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/telaInicial.css">
 </head>
 <body>
-    <?php
-        echo "<h1>Acessou o site $logado</h1>";
-    ?>
+<header>
+        <img id="logo" src="assets/img/logo.png" alt="Logo">
+        <nav>
+            <a href="">Treino</a>
+            <a href="">Dieta</a>
+            <a href="" id="suplementos">Suplementos</a>
+        </nav>
+        <?php
+           echo "<a id='usuario'>$nome</a>";
+        ?>
+
+    </header> 
+    
 </body>
 </html>
